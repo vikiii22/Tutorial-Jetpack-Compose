b8539e432fb93368d613a7a7fa29907c371e1d34
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2024-10-05 20:17:25 +0200

    change background colors and using material

--- app/src/main/java/com/example/tutorialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/tutorialjetpackcompose/MainActivity.kt
@@ -1,60 +1,75 @@
 package com.example.tutorialjetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tutorialjetpackcompose.ui.theme.TutorialJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponent()
+            TutorialJetpackComposeTheme() {
+                MyComponent()
+            }
         }
     }
 }
 
 @Composable
 fun MyTexts() {
-    Column() {
-        MyText("Hello World")
-        MyText("Estoy practicando Jetpack Compose")
+    Column(modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)) {
+        MyText("Hello World", MaterialTheme.colors.onBackground, MaterialTheme.typography.subtitle1)
+        Spacer(modifier = Modifier.height(16.dp))
+        MyText("Estoy practicando Jetpack Compose", MaterialTheme.colors.onBackground, MaterialTheme.typography.subtitle2)
     }
 }
 
 @Composable
-fun MyText(text: String) {
-    Text(text)
+fun MyText(text: String, color: Color, style: TextStyle) {
+    Text(text, color = color, style = style)
 }
 
 @Composable
 fun MyImage() {
     Image(
         painter = painterResource(id = R.drawable.ic_launcher_foreground),
-        contentDescription = "Android Logo"
+        contentDescription = "Android Logo",
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.primary)
+            .size(50.dp)
     )
 }
 
 @Composable
 fun MyComponent(){
-    Row {
+    Row(modifier = Modifier
+        .background(MaterialTheme.colors.background)
+        .padding(16.dp)
+    ) {
         MyTexts()
         MyImage()
     }
 }
 
-@Preview(showBackground = true)
+@Preview()
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun MyTextsPreview() {
     MyComponent()
